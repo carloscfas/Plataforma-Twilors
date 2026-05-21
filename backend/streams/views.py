15051a170474114186be8a25891f7f30719d036a
carloscfas <Carloasccasd15574@outlook.com>
2026-05-20 22:35:35 -0300

    feat: creating the permissions file and finalizing the view

--- backend/streams/views.py
+++ backend/streams/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from .models import Stream
 from .serializers import StreamSerializer
 from rest_framework import permissions, viewsets
+from .permissions import IsStreamerOrReadOnly
 
 # Create your views here.
 
 class StreamViewSet(viewsets.ModelViewSet):
     queryset = Stream.objects.all()
     serializer_class = StreamSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsStreamerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(streamer=self.request.user)
