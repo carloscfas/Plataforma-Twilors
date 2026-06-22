32a9538202408b50082b953c28e3421c62dfd0a9
Alvaro Veiga <alvarojoao17@gmail.com>
2026-06-21 21:46:45 -0300

    feat: add social-login route to accounts API

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, social_login
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('social-login/', social_login, name='social_login'),
 ]
